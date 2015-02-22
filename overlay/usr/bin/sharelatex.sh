767fba0e4f83b1c2653fd5ae2c96e65528fd5e95
sb-minimal <sb-minimal@c0n.de>
2015-02-22 14:49:23 +0100

    explicitly set path to config

--- overlay/usr/bin/sharelatex.sh
+++ overlay/usr/bin/sharelatex.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/texlive/2014/bin/x86_64-linux
+export SHARELATEX_CONFIG=/sharelatex/config/settings.development.coffee
 
 #does not seem to run as user - assume it is already started?
 #mongod &
